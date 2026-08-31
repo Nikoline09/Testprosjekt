14dd24d5fcebdbf232182f6fe7641e1a7136114f
Nikoline09 <09emmane@elev.danielsen-skoler.no>
2026-08-31 09:11:10 +0200

    spør om navn og printer det ut

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 #Oppgave: få terminal til å spørre om navnet og så
 #printe ut en velkomstmelding til dette navnet
 #prøv så å laste opp prosjektet på github
+print ("Hei, hva heter du?")
+y = input()
+print ("Velkommen til 2E, " + y +"!")
